0aeb2faa49c8a5c5218d76fab1985601009f8201
jerryamao <jerryamao.aj@gmail.com>
2023-03-20 19:11:15 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry poin
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
