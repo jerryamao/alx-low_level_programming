9b54c2d3be7367db03edf38cdaeb483cf77fe069
jerryamao <jerryamao.aj@gmail.com>
2023-03-22 06:10:57 -0700

    fixing main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int print_alphabet(char i)
+int print_alphabet(void)
 {
          /* Loop through the lowercase letters of the alphabet from 'a' to 'z' */
 	for (i = 'a' ; i <= 'z' ; i++)
 		 putchar(i);
          putchar('\n');
 	 return (0);
- }
+}
