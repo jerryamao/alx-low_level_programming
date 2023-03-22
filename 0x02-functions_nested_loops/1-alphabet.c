d7c810b1a761f299202b7244122a4cebe3c656b2
jerryamao <jerryamao.aj@gmail.com>
2023-03-22 16:39:51 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,10 @@
 void print_alphabet(void)
 {
 	char i;
-	/* Loop through the lowercase letters of the alphabet from 'a' to 'z' */
+
 	for (i = 'a' ; i <= 'z' ; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
-	return (0);
 }
