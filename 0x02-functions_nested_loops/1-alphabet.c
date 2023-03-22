d169da9e0feb49c232611348a02e53b724babcec
jerryamao <jerryamao.aj@gmail.com>
2023-03-22 05:15:05 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * alphabet - Prints the lowercase English alphabet to the console
+ *
+ * @i: Unused parameter (required by function signature)
+ *
+ * Return: Always 0
+ */
+
+int alphabet(char i)
+{
+	/* Loop through the lowercase letters of the alphabet from 'a' to 'z' */
+	for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Call the alphabet function to print the lowercase English alphabet */
+	alphabet('a');
+	return (0);
+}
