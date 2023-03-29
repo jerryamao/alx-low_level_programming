6f8584d3e61a83ad9b5c33408d6df11e4c8ca535
jerryamao <jerryamao.aj@gmail.com>
2023-03-28 18:50:17 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ *
+ * Return: no return.
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+}
